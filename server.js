604d6601ca4f18eb14292844bbe514638e38cda6
JordanLarson <jdlarson1234@gmail.com>
2020-08-11 14:37:56 -0700

    switching to mongoDB

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const logger = require("morgan");
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(logger("dev"));
+
+app.use("/api/users", usersController);
+app.use("/api/messages", messagesController);
+
+app.set("port", process.env.PORT || 3000);
+app.listen(app.get("port"), () => {
+  console.log(`✅ PORT: ${app.get("port")} 🌟`);
+});
