9cba2a19b350e68b72a69cc7f496c7521fd0a261
JordanLarson <jdlarson1234@gmail.com>
2020-08-15 19:40:51 -0700

    heroku pains

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const logger = require("morgan");
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 const usersController = require("./controllers/usersController");
 const spotsController = require("./controllers/spotsController");
 
